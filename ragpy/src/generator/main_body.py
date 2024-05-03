4cd39b2395736b270f4866da08358831bc6cf960
Sathvika-891 <90030506+Sathvika-891@users.noreply.github.com>
2024-05-03 11:37:38 +0530

    Update main_body.py

--- ragpy/src/generator/main_body.py
+++ ragpy/src/generator/main_body.py
@@ -1,18 +1,25 @@
 import sys
-from ragpy.src.generator.models_module import models_mod as mm
+import sys
+import sys
+from src.generator.models_module import models_mod as mm
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain.chains import RetrievalQA
-from ragpy.src.generator.prompt import CustomPromptTemplate
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+
+from src.generator.prompt import CustomPromptTemplate
 import pandas as pd
 from itertools import product
 import yaml
 import argparse
-
-
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma,FAISS
+import os
 class Generator_response():
     def __init__(self,db=None,retriever=None,query=None,
-                max_tokens = None,temperature= None,config = None):
+                max_tokens = None,temperature= None,config = None,embedding=None):
         """
         Initializes a new instance of the class.
 
@@ -22,12 +29,14 @@ class Generator_response():
             query (optional): The query object. Defaults to None.
             model_name (optional): The name of the model. Defaults to None.
         """
+        
         self.retriever = retriever
         self.db = db
         self.query = query
-        self.max_tokens = 249
+        self.max_tokens = max_tokens
         self.temperature = temperature
         self.data = config
+        self.embedding=embedding
     
     
     def format_docs(self,docs):
@@ -51,17 +60,25 @@ class Generator_response():
             retriever (object): The retriever object.
             datatype (str): The data type of the retriever object.
         """
+        
         if self.retriever == None:
-           retriever = self.db.as_retriever(search_type="similarity", search_kwargs={"k": 2})
-           return retriever,"document_type"
+            data=self.db.split("_")
+            db_name=data[-1]
+            if db_name.lower()=="chroma":
+                vectordb = Chroma(persist_directory=self.db, embedding_function=self.embedding)
+                self.db=vectordb
+            else:
+                vectordb = FAISS.load_local(self.db, embeddings=self.embedding)
+                self.db=vectordb    
+            retriever = self.db.as_retriever(search_type="similarity", search_kwargs={"k": 2})
+            return retriever,"document_type"
         else:
             retriever = self.retriever
-            # retriever = self.format_docs(retriever)
             return retriever, "string_datatype"
         
         
     def chains(self,retriever, prompt, models):
-        if data["chain_type"]=="simple":
+        if data["generator"]["chain_type"]=="simple":
             rag_chain = (
                 {"context": retriever | self.format_docs, "question": RunnablePassthrough()}
                 | prompt
@@ -77,28 +94,27 @@ class Generator_response():
             chain_type_kwargs={"prompt":prompt},
             return_source_documents=True)
         return rag_chain
-
-
+    
     def main(self, query):
         domain = self.data['generator']['prompt_template']['domain']
         prompt_type = self.data['generator']['prompt_template']['prompt_type']
         prompt = CustomPromptTemplate(domain)
         prompt = prompt.main(prompt_type)
         try:
-            retriever, datatype = self.retriever_fun()
+             retriever, datatype = self.retriever_fun()
         except Exception as e:
-            print(f"Error retrieving retriever: {e}")
-            return "both retriever and db are not provided."   
+            print(e)
+            return "both retriever and db are not provided"
+            
         objects = mm(config=self.data)
         models = {} # Dictionary to store model instances
         try:
             temp = self.data['generator']['model_config']['temperature']
-            # temp = [0.1,0.7]
             model_type = self.data['generator']['models']['model_type']
             if model_type == "openai":
                 model_list = self.data['generator']['models']['open_ai_model']
                 # model_list = ["gpt-3.5-turbo"]
-                combinations = product(temp, model_list)
+                combinations = list(product(temp, model_list))
 
                 for i in combinations:
                     model_key = f"openai_{i[1]}_{i[0]}" # Unique key for each model instance
@@ -129,35 +145,58 @@ class Generator_response():
             print(e)
             return "None"
 if __name__=="__main__":
-    with open('./config.yaml', 'r') as file:
+    path = "./config.yaml"
+    with open(path, 'r') as file:
         data = yaml.safe_load(file)
         # Argument parser setup
     parser = argparse.ArgumentParser(description='Generator Response for RAG')
-    parser.add_argument('--query', type=str, default='What are the considerations for treating elderly patients with SCLC?', help='The query for which main logic is executed.')
-    parser.add_argument('--context', type=str, default='[Document(page_content=\'viewed in th is light, that is, there must be a toxicity-to-beneﬁting the studies to be stopped early (250, 251).ratio.The optimal management of the elderly with SCLC is anA Phase II study of irinotecan plus cisplatin yielded a CR ofimportant issue as 40% of those who present with the disease29% and an overall response rate of 86% with a median survivalare over 70 years old. Studies that investigated this area suggestof 13.2 months in patients with extensive disease SCLC (269).that a reasonably high initial dose of chemotherapy is importantThis has led to an RCT of irinotecan and cisplatin versus etopo-and that the elderly tolerate radiotherapy well (274–276). In-side and cisplatin in patients with extensive disease SCLC. Thedeed, elderly patients with good performance status and normalstudy was halted early because of a signiﬁcant survival advantageorgan function do as well with optimal chemotherapy dosesfor the patients randomized to irinotecan plus cisplatin (medianas their younger\', metadata={\'page\': 103, \'source\': \'/content/drive/MyDrive/RAG BASED/MergedFiles1.pdf\'})]', help='The context for the query.')
-    parser.add_argument('--model_type', type=str, default='openai', help='The type of model to use. Can be "openai", "fireworks", or "hugging_face".')
+    parser.add_argument('--query', default='What is hello?', help='The query for which main logic is executed.')
+    parser.add_argument('--context', nargs='+', help='The context for which the query is asked.')
+    parser.add_argument('--model_type', type=str, default='hugging_face', help='The type of model to use. Can be "openai", "fireworks", or "hugging_face".')
     parser.add_argument('--chain_type', type=str, default='simple', help='The type of chain to use. Can be "simple", or "retrieval"')
     parser.add_argument('--domain', type=str, default='Healthcare', help='It can be anything.')
     parser.add_argument('--prompt_type', type=str, default='general', help='The type of prompt to use. Can be "general","custom" or "specific",')
-    
-    # Parse arguments
+    parser.add_argument('--temperature',nargs='+',default=[0.7,0.1],help="Temperature of the model. Default is 0.7.")
+    parser.add_argument('--repo_id',type=str,help="Hugging face repo id")
+    parser.add_argument('--embeddings', nargs='+', help="List of embedding options available are: huggingface_instruct_embeddings, all_minilm_embeddings, bgem3_embeddings, openai_embeddings")
+    parser.add_argument('--db_path',type=str,help="Path of the db")
+                        # Parse arguments
     args = parser.parse_args()
 
     # Check if provided arguments match expected values in the config file
-    if data["generator"]["models"]:
+    if args.model_type:
         data["generator"]["models"]["model_type"] = args.model_type
-    if data["generator"]["chain_type"]:
+    if args.chain_type:
         data["generator"]["chain_type"] = args.chain_type   
-    if data["generator"]["prompt_template"]["domain"]:   
+    if args.domain:   
         data["generator"]["prompt_template"]["domain"] = args.domain   
-    if data["generator"]["prompt_template"]["prompt_type"]:  
+    if args.prompt_type:  
         data["generator"]["prompt_template"]["prompt_type"] = args.prompt_type
-
-    # Create an instance of the Generation_Benchmarking class and run the benchmarks
-    rag_object = Generator_response(retriever= args.context,config=data)
-    results = rag_object.main(args.query) 
-   
-    print("---------------------------------------------------------------------------------------------------")
-    print("---------------------------------------------------------------------------------------------------")
-    print("Results from model:")
-    print(results)
+    if args.temperature:
+        data["generator"]["model_config"]["temperature"]=args.temperature
+    if args.repo_id:
+        data["generator"]["models"]["hugging_face_model"]=args.repo_id
+    if args.embeddings:
+        data["retriever"]["vector_store"]["embeddings"] = args.embeddings
+    if args.db_path:
+        data["retriever"]["vector_store"]["persist_directory"] = args.db_path
+    
+    if args.context:
+        rag_object = Generator_response(retriever=args.context,config=data,query=args.query,db=data["retriever"]["vector_store"]["persist_directory"])
+    else:
+        if args.embedding.lower()=="openai_embeddings":
+            embedding_function = OpenAIEmbeddings()
+        elif args.embedding.lower()=="huggingface_instruct_embeddings":
+            embedding_function = HuggingFaceInstructEmbeddings()
+        elif args.embedding.lower()=="all_minilm_embeddings":
+            embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+        elif args.embedding.lower()=="bgem3_embeddings":
+            embedding_function = HuggingFaceEmbeddings(model_name="BAAI/bge-large-en-v1.5")
+        else:
+            raise ValueError("Invalid embedding name. Please choose from: openai_embeddings, huggingface_instruct_embeddings, all_minilm_embeddings, bgem3_embeddings")
+        if args.db_path:
+            rag_object = Generator_response(config=data,query=args.query,db=args.db_path,embedding=embedding_function)
+        else:
+            raise ValueError(""both retriever and db path are not provided")
+    results = rag_object.main(args.query)
+    print("Results from model:",results)
