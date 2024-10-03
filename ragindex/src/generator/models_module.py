c57faf7dce262d40d4e8f86a73ea2984c2bc9380
AbhinayaVeerapannei <168399669+AbhinayaVeerapannei@users.noreply.github.com>
2024-10-03 12:12:53 +0530

    Update models_module.py

--- ragindex/src/generator/models_module.py
+++ ragindex/src/generator/models_module.py
@@ -8,6 +8,9 @@ from huggingface_hub import InferenceApi
 from dotenv import load_dotenv
 import warnings
 import sys 
+import logging
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+
 
 class models_mod():
     def __init__(self,max_tokens=256,config=None):
@@ -21,8 +24,8 @@ class models_mod():
             llm = ChatOpenAI(model=model_name,temperature=temp) 
             return llm
         except Exception as e:  
-            print(e) 
-            sys.exit(1)  # Exit with a non-zero status code
+            logging.info(e) 
+            sys.exit(1)
             
 
     def hugging_face(self,temp=None,model_name=None):
@@ -31,21 +34,21 @@ class models_mod():
             model_location = f"{url}/{model_name}"
             response = requests.get(model_location)
             if response.status_code == 200:
-                print("response", response.status_code)
+                logging.info("response", response.status_code)
                 result = response.json()
                 if result["gated"] == False:
-                    print(temp)
+                    logging.info(temp)
                     llm = HuggingFaceEndpoint(repo_id=model_name, max_new_tokens=249, temperature=temp)
                     return llm
                 else:
-                    print("The repo is gated. Try with another one.")
-                    sys.exit(1)  # Exit with a non-zero status code
+                    logging.info("The repo is gated. Try with another one.")
+                    sys.exit(1)
             else:
-                print("Check for the API or maybe your internet connection is wonky.")
-                sys.exit(1)  # Exit with a non-zero status code
+                logging.info("Check for the API or maybe your internet connection is wonky.")
+                sys.exit(1)
         except Exception as e:
-            print(f"An error occurred: {e}")
-            sys.exit(1)  # Exit with a non-zero status code
+            logging.info(f"An error occurred: {e}")
+            sys.exit(1)
 
     def main(self,model_type,model_name,temp):
         if model_type=="openai":
@@ -56,7 +59,6 @@ class models_mod():
                 models = []
                 for t in temp:
                     models.append(self.hugging_face(t, model_name))
-            #model = self.hugging_face(str(temp),model_name)
                 return models 
             else:
                 return self.hugging_face(temp, model_name)
@@ -65,18 +67,16 @@ class models_mod():
 if __name__ == "__main__": 
     with open('./config.yaml', 'r') as file:
         data = yaml.safe_load(file)
-    # Create the parser
     parser = argparse.ArgumentParser(description='Select the model type for the generator.')
     parser.add_argument('--model_type', type=str,default = "openai", choices=['openai', 'hugging_face'],
                         help='The type of model to use. Can be "openai", "hugging_face"')
     parser.add_argument('--model_name', type=str, default="gpt-3.5-turbo", help='The name of the model to use.')
     parser.add_argument('--temperature', type=float, nargs='+', default=0.7, help="Temperature of the model. Default is 0.7.")
 
-    # Parse the arguments
     args = parser.parse_args()
 
-    print(args.model_type)
-    print(args.model_name)
+    logging.info(args.model_type)
+    logging.info(args.model_name)
     if args.model_type:
         data["generator"]["models"]["model_type"] = args.model_type
     if args.model_name:
@@ -86,11 +86,10 @@ if __name__ == "__main__":
 
     method=models_mod(data)
     llms={}
-    # handle multiple temperatures
     if len(data["generator"]["model_config"]["temperature"])==1:
         temp=data["generator"]["model_config"]["temperature"]
         llms[temp[0]]=method.main(data["generator"]["models"]["model_type"], data["generator"]["models"]["model_name"], temp=temp[0])
     else:
         for temo in data["generator"]["model_config"]["temperature"]:
             llms[temo]=method.main(data["generator"]["models"]["model_type"],data["generator"]["models"]["model_name"],temp=temo)
-    print("llms are loaded", llms)
+    logging.info("llms are loaded", llms)
